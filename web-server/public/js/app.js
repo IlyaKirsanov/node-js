777bc0d4ddfec6e346f67dbf7509cc2bb07dae16
IlyaKirsanov <ilya.kirsanov@gmail.com>
2019-06-26 22:21:03 +0300

    query search, integretion geocoding and forecast; cleint side data fetching

---
+++ web-server/public/js/app.js
@@ -0,0 +1,29 @@
+console.log('Client side works');
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+weatherForm.addEventListener('submit', (event)=>{
+    event.preventDefault();
+
+    const location = search.value;
+
+    messageOne.textContent = 'Getting forecast...';
+    messageTwo.textContent = '';
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response)=>{
+    response.json().then((data)=>{
+        setTimeout(()=>{
+            if(data.error){
+                messageOne.textContent = data.error;
+            }else{
+                messageOne.textContent = data.location; 
+                messageTwo.textContent = data.forecast;
+            }
+        },2000)
+            
+    })
+})
+})
